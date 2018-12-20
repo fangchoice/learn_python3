665f87a0792d7022fffc2d36eda901546b94d74d
fangchoice <625486143@qq.com>
2018-12-20 17:34:21 +0800

    Create ospath_join.py

---
+++ fileSystem/ospath/ospath_join.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os.path
+
+PATHS = [
+	('one', 'two', 'three'),
+	('/', 'one', 'two', 'three'),
+	('/one', '/two', '/three'),
+]
+
+for parts in PATHS:
+	print('{} : {!r}'.format(parts, os.path.join(*parts)))
