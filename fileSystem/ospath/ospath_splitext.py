1b8481ef7aa4b25ae828edb9f32af3dd7c385db5
fangchoice <625486143@qq.com>
2018-12-20 18:18:07 +0800

    Create ospath_splitext.py

---
+++ fileSystem/ospath/ospath_splitext.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os.path
+
+PATHS = [
+	'filename.txt',
+	'filename',
+	'/path/to/filename.txt',
+	'/',
+	'',
+	'my-archive.tar.gz',
+	'no-extension.',
+]
+
+for path in PATHS:
+	print('{!r:>21} : {!r}'.format(path, os.path.splitext(path)))
