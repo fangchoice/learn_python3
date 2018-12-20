8f15bd8ccde19d001241d062aae7a07574367c0a
fangchoice <625486143@qq.com>
2018-12-20 18:10:52 +0800

    Create ospath_normpath.py

---
+++ fileSystem/ospath/ospath_normpath.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+import os.path
+
+PATHS = [
+	'one/two//three',
+	'one/.two/.three',
+	'one/../alt/two/three',
+]
+
+for path in PATHS:
+	print('{!r:>22} : {!r}'.format(path, os.path.normpath(path)))
