16270837e8f867215b8bd4f03b1b6aba1d3b9c39
fangchoice <625486143@qq.com>
2018-12-20 17:22:44 +0800

    Update ospath_basename.py

--- fileSystem/ospath/ospath_basename.py
+++ fileSystem/ospath/ospath_basename.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-# basename()
+# basename() 函数返回一个等于值的第二部分的split()值
 
 import os.path
+PATHS = [
+	'/one/two/three',
+	'/one/two/three/',
+	'/',
+	'.',
+	'',
+]
+
+for path in PATHS:
+	print('{!r:>17} : {!r}'.format(path, os.path.basename(path)))
